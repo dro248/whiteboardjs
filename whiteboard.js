7790979e100a716c7d9a79b96c75bcb3fe0fa90c
David Ostler <david.ostler001@gmail.com>
2018-08-26 16:06:58 -0600

    crude drawing works (lines only)

--- whiteboard.js
+++ whiteboard.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var whiteboard;
+var whiteboard, hammer
 
 class Whiteboard {
 
@@ -8,35 +8,107 @@ class Whiteboard {
 		console.info("Whiteboard constructor()");
 		this.canvas = document.getElementById("whiteboard_canvas");
 		this.context = this.canvas.getContext("2d");
-		this.color = null
-		this.mouseOver = false
+		this.color = 'red'
+		this.touchActive = false
+		this.lastTouchPoint = null
 
 		// Create event listeners
-		this.canvas.addEventListener('onmouseover', (event) => { 
-			console.log("mouse over")
-			this.mouseOver = true 
-		})
-		this.canvas.addEventListener('onmouseout', (event) => { 
-			console.log("mouse left")
-			this.mouseOver = false 
-		})
-		this.canvas.addEventListener('onmousemove', (event) => { 
-			console.log("mouse moved!")
-			// if (!mouseOver) { return }
+		this.canvas.addEventListener('touchstart', (event) => 	{ this.handleTouchStart(event) })
+		this.canvas.addEventListener('touchend', (event) => 	{ this.handleTouchEnd(event) })
+		this.canvas.addEventListener('touchmove', (event) => 	{ this.handleTouchMove(event) })
+		this.canvas.addEventListener('touchcancel', (event) => 	{ this.handleTouchCancel(event) })
 
-			// console.log(event)
-		})
-
+		// On init, resize the canvas to fit the window
 		this.resize(window, this.canvas)
 	}
 
+	handleTouchStart(event){
+		console.log("touchstart")
+		this.touchActive = true 
+		let touch = this.getTouchList(event)[0]
+		this.lastTouchPoint = touch
+	}
+
+	handleTouchEnd(event){
+		console.log("touchend")
+		this.touchActive = false 
+		this.lastTouchPoint = null
+	}
+
+	handleTouchMove(event){
+		let touchList = this.getTouchList(event)
+
+		// NOTE: We only want to draw if we have ONE draw point
+		if(touchList.length === 0 || touchList.length > 1){
+			return
+		}
+
+		event.preventDefault()
+
+		if (this.touchActive){
+			this.draw(touchList[0], this.canvas, this.color, this.lastTouchPoint)
+		}
+	}
+
+	handleTouchCancel(event){
+		// TODO
+		console.warn("TouchCancel() Triggered!")
+	}
 
 	resize() {
+		// Resize the whiteboard to fill the window
 		this.canvas.height = window.innerHeight
 		this.canvas.width = window.innerWidth
 		
 		console.log('canvas height: ' + String(this.canvas.height))
 		console.log('canvas width: ' + String(this.canvas.width))
+	}
+
+	draw(touch, canvas, color) {
+
+
+		if (!this.lastTouchPoint){
+			console.error("You're hitting me!")
+			this.lastTouchPoint = touch
+			return
+		}
+
+
+		console.info("draw line: " + color)
+		console.log("touch")
+		console.log(touch)
+		console.log("lastTouchPoint")
+		console.log(this.lastTouchPoint)
+
+		console.log("canvas")
+		console.log(canvas)
+
+
+		let ctx = canvas.getContext("2d");
+		ctx.beginPath();
+		ctx.moveTo(this.lastTouchPoint.clientX, this.lastTouchPoint.clientY);
+		ctx.lineTo(touch.clientX, touch.clientY);
+		// ctx.moveTo(0,0);
+		// ctx.lineTo(1500, 1500);
+		ctx.stroke();
+
+		this.lastTouchPoint = touch
+	}
+
+	getTouchList(event){
+		// Returns an Array of touch objects
+
+		console.log(event['changedTouches'])
+
+		if (!event['changedTouches']){
+			return
+		}
+
+		let touches = []
+		for (let i = 0; i < event.changedTouches.length; i++){
+			touches.push(event['changedTouches'][String(i)])
+		}
+		return touches
 	}
 }
 
@@ -48,7 +120,6 @@ window.addEventListener("load", () => {
 
 window.addEventListener("resize", () => {
 	if (whiteboard){
-		// debugger;
 		whiteboard.resize(window)
 	}
 });
